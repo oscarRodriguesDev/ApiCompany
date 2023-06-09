119047d40311fe9c8855bda954777800ffd304a5
Oscar Rodrgues <oscar.rodrigues.dev@gmail.com>
2023-06-09 12:57:33 -0300

    função de recuperação de areas por empresa definida e funcionando

--- server.js
+++ server.js
@@ -3,31 +3,67 @@
 
 const http = require('http');
 const sqlite = require('sqlite3').verbose();
-const db = new sqlite.Database('sqlite-sql/Companies.db');
 
 
-//SELECT nomeEmpresa FROM empresa
-//função para fazer uma consulta sql
-// Função para fazer uma consulta SQL
-function consultar(termo, tabela) {
+
+
+
+
+/**Função para consulta simples de sql */
+function consultar(termo, tabela, valores, callback) {
+    const db = new sqlite.Database('sqlite-sql/Companies.db');
     const query = 'SELECT ' + termo + ' FROM ' + tabela;
     db.all(query, [], (err, rows) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-
-        // Iterar sobre as linhas retornadas
-        rows.forEach((row) => {
-            const valorColuna = row[termo]; // Acessar o valor da coluna dinamicamente
-            console.log('Valor da coluna: ' + valorColuna);
-        });
-
-        // Fechar o banco de dados após a conclusão da consulta
-        db.close();
-    });
-}
+      if (err) {
+        console.error(err);
+        callback(err);
+        return;
+      }
   
+      rows.forEach((row, index) => {
+        const valorColuna = row[termo];
+        valores[index + 1] = valorColuna;
+      });
+  
+      db.close();
+  
+      callback(null);
+    });
+  }
+  
+
+
+  //consulta para usar duas tabelas
+  function consulta_complex(empresa, valores, callback) {
+    const db = new sqlite.Database('sqlite-sql/Companies.db');
+    var query = `SELECT empresa.nomeEmpresa, areas.nomeArea
+                 FROM empresa
+                 INNER JOIN areas ON empresa.idEmpresa = areas.idEmpresa
+                 WHERE empresa.nomeEmpresa = ?`;
+  
+    db.all(query, [empresa], (err, rows) => {
+      if (err) {
+        console.error(err);
+        callback(err);
+        return;
+      }
+  
+      rows.forEach((row, index) => {
+        const valorColuna = row['nomeArea'];
+        valores[index + 1] = valorColuna;
+      });
+  
+      db.close();
+  
+      callback(null);
+    });
+  }
+  
+
+
+
+
+
 
 
 //inicianso o serividor
@@ -36,80 +72,99 @@ const server = http.createServer((req, res) => {
 
     //servidor analisa a url e responde de acordo com cada solicitação
     // caso a solicitação seja para listar empresas
-    if (url === '/api/listEmpresas' && method === 'GET'){
-        consultar('nomeEmpresa', 'empresa');
-        
-           }
+    if (url === '/api/listEmpresas' && method === 'GET') {
+        const valores = {};
+        consultar('nomeEmpresa', 'empresa', valores, (err) => {
+          if (err) {
+            console.error('Ocorreu um erro:', err);
+            // Lógica para lidar com o erro, se necessário
+            return;
+          }
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.end(JSON.stringify(valores));
+          //console.log(valores);
+        });
+      }
+      
 
     //listar todas as areas
     else if (url === '/api/listAreas' && method === 'GET') {
-        console.log('LISTAR AREAS')
-        consultar('nomeArea','areas')
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({ message: 'listar empresas com sql' }))
+        const valores = {};
+        consultar('nomeArea', 'areas', valores, (err) => {
+          if (err) {
+            console.error('Ocorreu um erro:', err);
+            // Lógica para lidar com o erro, se necessário
+            return;
+          }
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.end(JSON.stringify(valores));
+          //console.log(valores);
+        });
     }
 
-    //caso a solicitação seja para criar uma nova empresa
-    else if (url === '/api/areaPorEmpresa' && method === 'GET') {
-        console.log('area por empresa')
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({ message: 'MOSTRANDO DADOS DA EMPRESA XXX' }));
 
+
+    //caso a solicitação seja para listar as areas por empresa
+    else if (url.includes('/api/areaPorEmpresa') && req.method === 'GET') {
+      //var nomeEmpresa = 'A&D-software'
+      const myURL = new URL(`http://${req.headers.host}${req.url}`);
+      const queryParams = myURL.searchParams;
+      const nomeEmpresa = queryParams.get('nomeEmpresa');
+
+      console.log('Dados capturados:', nomeEmpresa);
+      var valores = {}
+      const url = `http://${req.headers.host}${req.url}`;
+      console.log('URL completa:', url);
+
+      consulta_complex(nomeEmpresa, valores, function () {
+        console.log(valores)
+      })
     }
 
     //caso a solicitação seja para atualizar empresas
     else if (url === '/api/updateEmpresas' && method === 'GET') {
-        console.log('ATUALIZAR EMPRESAS')
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({ message: 'ATUALIZANDO EMPRESAS' }));
-
+         //...
+      //
 
     }
 
 
     //caso a solicitação seja para atualizar areas
     else if (url === '/api/updateAreas' && method === 'GET') {
-        console.log('ATUALIZAR AREAS')
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({ message: 'ATUALIZANDO AS AREAS' }));
+       //...
+      //
 
     }
 
     //caso a solicitação seja para criar uma nova empresa
     else if (url === '/api/novaEmpresa' && method === 'GET') {
-        console.log('NOVA EMPRESA')
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({ message: 'NOVA EMPRESA' }));
+        //...
+      //
 
     }
 
     //caso a solicitação seja para criar uma nova area
     else if (url === '/api/novaArea' && method === 'GET') {
-        console.log('NOVA AREA')
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({ message: 'NOVA AREA' }));
+        //...
+      //
 
     }
 
     //caso a solicitação não seja encontrada ou não possa ser atendida 
     else {
         console.log('pagina 404')
-        res.statusCode = 404;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Rota nao encontrada');
+   
     }
 });
+
+
 
 //aqui é definido a porta onde o servidor será ouvido
 const port = 3000;
 const hostname = 'localhost';
-const defaultRoute = '/api/listEmpresas'; // Rota inicial desejada
+const defaultRoute = '/api/listEmpresas';
 console.log('atualizando automaticamente o servidor')
 
 server.listen(port, hostname, () => {
