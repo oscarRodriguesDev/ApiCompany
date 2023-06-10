1da7ec3c87757bf2c86ce76662c4c76fdcfc7a50
Oscar Rodrgues <oscar.rodrigues.dev@gmail.com>
2023-06-10 16:15:56 -0300

    criação de uma nova função para recuperar areas, listar empresas e listar areas

--- server.js
+++ server.js
@@ -1,30 +1,87 @@
-//modulo necessario para trabalhar com requisições http
-
-
 const http = require('http');
 const sqlite = require('sqlite3').verbose();
 
+/**Função para listar as empresas e suas descrições */
+function listarEmpresas(callback) {
+  const db = new sqlite.Database('./sqlite-sql/Companies.db');
 
+  const selectQuery = 'SELECT nomeEmpresa, descricaoEmpresa FROM empresa';
 
-/**Função para consulta simples de sql */
-function consultar(termo, tabela, valores, callback) {
-  const db = new sqlite.Database('sqlite-sql/Companies.db');
-  const query = 'SELECT ' + termo + ' FROM ' + tabela;
-  db.all(query, [], (err, rows) => {
+  db.all(selectQuery, (err, rows) => {
     if (err) {
       console.error(err);
-      callback(err);
+      callback(err, null);
       return;
     }
 
-    rows.forEach((row, index) => {
-      const valorColuna = row[termo];
-      valores[index + 1] = valorColuna;
+    const empresas = {};
+    rows.forEach((row) => {
+      empresas[row.nomeEmpresa] = row.descricaoEmpresa;
     });
 
-    db.close();
+    callback(null, empresas);
 
-    callback(null);
+    db.close();
+  });
+}
+
+
+//função para listar as areas e suas respectivas descrições
+function listarAreas(callback) {
+  const db = new sqlite.Database('./sqlite-sql/Companies.db');
+
+  const selectQuery = 'SELECT nomeArea, descricaoArea FROM areas';
+
+  db.all(selectQuery, (err, rows) => {
+    if (err) {
+      console.error(err);
+      callback(err, null);
+      return;
+    }
+
+    const areas = {};
+    rows.forEach((row) => {
+      areas[row.nomeArea] = row.descricaoArea;
+    });
+
+    callback(null, areas);
+
+    db.close();
+  });
+}
+
+
+
+//função para listar todas as empresas por area
+function listarEmpresasPorAreas(callback) {
+  const db = new sqlite.Database('./sqlite-sql/Companies.db');
+
+  const selectQuery = `
+    SELECT empresa.nomeEmpresa, areas.nomeArea
+    FROM empresa
+    LEFT JOIN areas ON empresa.idEmpresa = areas.idEmpresa
+    ORDER BY empresa.nomeEmpresa
+  `;
+
+  db.all(selectQuery, (err, rows) => {
+    if (err) {
+      console.error(err);
+      callback(err, null);
+      return;
+    }
+
+    const empresasPorAreas = {};
+    rows.forEach((row) => {
+      const { nomeEmpresa, nomeArea } = row;
+      if (!empresasPorAreas[nomeEmpresa]) {
+        empresasPorAreas[nomeEmpresa] = [];
+      }
+      empresasPorAreas[nomeEmpresa].push(nomeArea);
+    });
+
+    callback(null, empresasPorAreas);
+
+    db.close();
   });
 }
 
@@ -100,6 +157,7 @@ function pushEnterprise(nomeEmpresa, descEmpresa, nomeArea, descArea) {
   db.close();
 }
 
+
 //função para deletar empresas  completas
 function deleteCompany(nomeEmpresa) {
   const db = new sqlite.Database('sqlite-sql/Companies.db');
@@ -123,49 +181,190 @@ function deleteCompany(nomeEmpresa) {
 }
 
 
+//função para adcionar areas 
+function inserirArea(nomeEmpresa, nomeArea, descricaoArea) {
+  const db = new sqlite.Database('sqlite-sql/Companies.db');
+  const query = 'SELECT idEmpresa FROM empresa WHERE nomeEmpresa = ?';
+  db.get(query, [nomeEmpresa], function (err, row) {
+    if (err) {
+      console.error('ocorreu o erro ' + err);
+      return;
+    }
+    if (!row) {
+      console.log('Empresa não encontrada!');
+      return;
+    }
+    const idEmpresa = row.idEmpresa;
+    const insertQuery = 'INSERT INTO areas (nomeArea, descricaoArea, idEmpresa) VALUES (?, ?, ?)';
+    db.run(insertQuery, [nomeArea, descricaoArea, idEmpresa], function (err) {
+      if (err) {
+        console.error('ocorreu o erro ' + err);
+        return;
+      }
+      console.log('Área inserida com sucesso!');
+    });
+  });
+  db.close();
+}
+
+
+/**Função para deletar area de determinada empresa */
+function deleteArea(nomeEmpresa, nomeArea) {
+  const db = new sqlite.Database('./sqlite-sql/Companies.db');
+  const queryEmpresa = 'SELECT idEmpresa FROM empresa WHERE nomeEmpresa = ?';
+  db.get(queryEmpresa, [nomeEmpresa], function (err, rowEmpresa) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    if (!rowEmpresa) {
+      console.log('Empresa não encontrada!');
+      return;
+    }
+    const idEmpresa = rowEmpresa.idEmpresa;
+    const deleteQuery = 'DELETE FROM areas WHERE idEmpresa = ? AND nomeArea = ?';
+    db.run(deleteQuery, [idEmpresa, nomeArea], function (err) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log('Área deletada com sucesso!');
+    });
+  });
+
+  db.close();
+}
+
+
+/**Função para alterar descrição da area de uma empresa */
+function updateArea(nomeEmpresa, nomeArea, novaDescricaoArea) {
+  const db = new sqlite.Database('./sqlite-sql/Companies.db');
+  const queryEmpresa = 'SELECT idEmpresa FROM empresa WHERE nomeEmpresa = ?';
+  db.get(queryEmpresa, [nomeEmpresa], function (err, rowEmpresa) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    if (!rowEmpresa) {
+      console.log('Empresa não encontrada!');
+      return;
+    }
+    const idEmpresa = rowEmpresa.idEmpresa;
+    const queryArea = 'SELECT nomeArea FROM areas WHERE idEmpresa = ? AND nomeArea = ?';
+    db.get(queryArea, [idEmpresa, nomeArea], function (err, rowArea) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      if (!rowArea) {
+        const insertQuery = 'INSERT INTO areas (nomeArea, descricaoArea, idEmpresa) VALUES (?, ?, ?)';
+        db.run(insertQuery, [nomeArea, novaDescricaoArea, idEmpresa], function (err) {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          console.log('Nova área criada com sucesso!');
+        });
+      } else {
+        const updateQuery = 'UPDATE areas SET descricaoArea = ? WHERE idEmpresa = ? AND nomeArea = ?';
+        db.run(updateQuery, [novaDescricaoArea, idEmpresa, nomeArea], function (err) {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          console.log('Área atualizada com sucesso!');
+        });
+      }
+    });
+  });
+  db.close();
+}
+
+
+
+/**Função para alterar area da empresa informada*/
+function updateEmpresa(nomeEmpresa, novaDescricao) {
+  const db = new sqlite.Database('./sqlite-sql/Companies.db');
+  const query = 'UPDATE empresa SET descricaoEmpresa = ? WHERE nomeEmpresa = ?';
+  db.run(query, [novaDescricao, nomeEmpresa], function(err) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    if (this.changes === 0) {
+      console.log('Empresa não encontrada!');
+      return;
+    }
+  });
+  db.close();
+}
+
+
 
 /**instancia do servidor para definição das rotas da api */
 const server = http.createServer((req, res) => {
   const { url, method } = req;
 
-  /**Essa rota lista todas as empresas do banco de dados */
+  /**Essa rota lista todas as empresas e suas descrições do banco de dados */
   if (url === '/api/listEmpresas' && method === 'GET') {
-    const valores = {};
-    consultar('nomeEmpresa', 'empresa', valores, (err) => {
+    const empresas = {};
+    listarEmpresas((err, empresas) => {
       if (err) {
-        console.error('Ocorreu um erro:', err);
-        // Lógica para lidar com o erro, se necessário
+        console.error(err);
         return;
       }
+    
+      console.log(empresas);
+    
+      // Faça o que quiser com o objeto `empresas` aqui
+      
       res.statusCode = 200;
       res.setHeader('Content-Type', 'application/json');
-      res.end(JSON.stringify(valores));
-     console.log(valores)
-     //falta apenas recuperar os dados na pagina tambem
+      res.end(JSON.stringify(empresas));
+      console.log(empresas)
+      //falta apenas recuperar os dados na pagina tambem
     });
+   
   }
+
 
 
   /**Essa rota lista todas as areas contidas no Banco de dados */
   else if (url === '/api/listAreas' && method === 'GET') {
-    const valores = {};
-    consultar('nomeArea', 'areas', valores, (err) => {
+    const areas = {};
+    listarAreas((err, areas) => {
       if (err) {
-        console.error('Ocorreu um erro:', err);
-        // Lógica para lidar com o erro, se necessário
+        console.error(err);
         return;
       }
       res.statusCode = 200;
       res.setHeader('Content-Type', 'application/json');
-      res.end(JSON.stringify(valores));
-      console.log(valores);
-      //só falta recuperar os dados na pagina
+      res.end(JSON.stringify(areas));
+      console.log(areas);  
     });
+    
+    
   }
 
 
+  /**Essa rota recupera todas as areas por empresa */
+  else if (url === '/api/areasGeral' && method === 'GET') {
+    var areasPorEmpresa={}
+    listarEmpresasPorAreas((err, areasPorEmpresa) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log(areasPorEmpresa);
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(areasPorEmpresa));
+      console.log(areasPorEmpresa);  
+    });
+  }
 
-/**Essa rota permite visualizar as areas por empresa */
+  
+  /**Essa rota permite visualizar as areas da empresa selecionada */
   else if (url.includes('/api/areaPorEmpresa') && req.method === 'GET') {
     const myURL = new URL(`http://${req.headers.host}${req.url}`);
     const queryParams = myURL.searchParams;
@@ -182,24 +381,38 @@ const server = http.createServer((req, res) => {
       res.setHeader('Content-Type', 'application/json');
       res.end(JSON.stringify({ 'nomeEmpresa': valores['nomeEmpresa'], 'nomeArea': valores['nomeArea'] }));
       console.log(valores)
-    // ao  invés de mostrar os dados em aberto num paragrafo desejo mostrar os dados numa tabela
+      // ao  invés de mostrar os dados em aberto num paragrafo desejo mostrar os dados numa tabela
     })
   }
 
-  //caso a solicitação seja para atualizar empresas
-  else if (url === '/api/updateEmpresas' && method === 'GET') {
-    //...
-    //
 
+
+  /**Função para alterar a descrição de uma empresa */
+  else if (url.includes('/api/updateEmpresa') && req.method === 'GET') {
+    //urlExample:/api/updateEmpresas/?nomeEmpresa=nome&descricao=novadescricao
+    const myURL = new URL(`http://${req.headers.host}${req.url}`);
+    const queryParams = myURL.searchParams;
+    const nomeEmpresa = queryParams.get('nomeEmpresa');
+    const novaDesc = queryParams.get('descricao');
+    updateEmpresa(nomeEmpresa,novaDesc)
+    console.log('A empresa '+nomeEmpresa+' teve sua descrição alterada para '+novaDesc)
   }
 
 
-  //caso a solicitação seja para atualizar areas
-  else if (url === '/api/updateAreas' && method === 'GET') {
-    //...
-    //
 
+  /**Rota para atualizar areas da empresa informada */
+  else if (url.includes('/api/updateArea') && req.method === 'GET') {
+    //urlExample: /api/updateAreas/?nomeEmpresa=empresa&nomeArea=area&novaDesc=nova descriação a ser adcionada
+    const myURL = new URL(`http://${req.headers.host}${req.url}`);
+    const queryParams = myURL.searchParams;
+    const nomeEmpresa = queryParams.get('nomeEmpresa');
+    const nomeArea = queryParams.get('nomeArea');
+    const novaDesc = queryParams.get('novaDesc');
+    updateArea(nomeEmpresa, nomeArea, novaDesc)
+    console.log('A descrição ' + novaDesc + ' foi adcionada na area de ' + nomeArea + ' da empresa ' + nomeEmpresa);
   }
+
+
 
   /** essa rota permite criação de empresas */
   else if (url.includes('/api/novaEmpresa') && req.method === 'GET') {
@@ -217,22 +430,43 @@ const server = http.createServer((req, res) => {
 
   }
 
-  //caso a solicitação seja para criar uma nova area
-  else if (url === '/api/novaArea' && method === 'GET') {
-    //...
-    //
+
+
+  /**rota para incluir novas areas nas empresas */
+  else if (url.includes('/api/novaArea') && req.method === 'GET') {
+    //url:example = /api/novaArea/?nomeEmpresa=Empresa2&nomeArea=RH&descArea=Recursos Humanos
+    const myURL = new URL(`http://${req.headers.host}${req.url}`);
+    const queryParams = myURL.searchParams;
+    const nomeEmpresa = queryParams.get('nomeEmpresa');
+    const nomeArea = queryParams.get('nomeArea');
+    const descArea = queryParams.get('descArea');
+    console.log('Dados capturados:', nomeEmpresa, nomeArea, descArea)
+    inserirArea(nomeEmpresa, nomeArea, descArea)
 
   }
 
-  //nova rota para deletar uma empresa informa
-  //falta resolver a forma de pegar pela url
+
+
+  /**rota para deletar empresas */
   else if (url.includes('/api/deleteCompany') && req.method === 'GET') {
     const myURL = new URL(`http://${req.headers.host}${req.url}`);
     const queryParams = myURL.searchParams;
     const nomeEmpresa = queryParams.get('nomeEmpresa');
-    deleteCompany(nomeEmpresa) 
+    deleteCompany(nomeEmpresa)
   }
 
+
+
+  /**Rota para deletar uma determinada area de uma empresa */
+  else if (url.includes('/api/deleteArea') && req.method === 'GET') {
+    //urlExample: /api/deleteArea/?nome=Empresa2&nomeArea=TI
+    const myURL = new URL(`http://${req.headers.host}${req.url}`);
+    const queryParams = myURL.searchParams;
+    const nomeEmpresa = queryParams.get('nomeEmpresa');
+    const nomeArea = queryParams.get('nomeArea');
+    deleteArea(nomeEmpresa, nomeArea)
+    console.log('Area de ' + nomeArea + ' da empresa ' + nomeEmpresa + ' foi deletada com sucesso!')
+  }
 
 
   /**caso a rota não seja encontrada somos encaminhados para a pagina de erro 404 */
